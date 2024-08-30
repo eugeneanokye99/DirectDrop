e24989116247f6186eb251f4f678628a76d6637c
Jmint <jeffreymintah737@gmail.com>
2024-08-30 16:56:00 +0000

    changes

--- Frontend/directdrop/src/components/Navbar.jsx
+++ Frontend/directdrop/src/components/Navbar.jsx
@@ -27,7 +27,11 @@ const Navbar = () => {
                     </a>
                 </div>
                 <div>
-                    <a href="#responsive-header" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">
+                    <a href="#responsive-header" className="inline-block 
+                    text-sm px-4 py-2 leading-none border 
+                    rounded text-white border-white 
+                    hover:border-transparent 
+                    hover:text-blue-500 hover:bg-white mt-4 lg:mt-0">
                         Sign In
                     </a>
                 </div>
