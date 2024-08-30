2342c6ae749d51054dff9cd3774cced4f5195343
Jmint <jeffreymintah737@gmail.com>
2024-08-30 16:53:05 +0000

    changes

--- Frontend/directdrop/src/App.jsx
+++ Frontend/directdrop/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Navbar} from './components/Navbar'
 import { Button} from '@chakra-ui/react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="p-4">
       <Router>
+      <Navbar />
         <Routes>
          
         </Routes>
