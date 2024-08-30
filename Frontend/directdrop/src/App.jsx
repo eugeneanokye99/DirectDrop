82da85503d6b422f311f9521d7884795daacbfa1
Jmint <jeffreymintah737@gmail.com>
2024-08-30 16:52:26 +0000

    changes

--- Frontend/directdrop/src/App.jsx
+++ Frontend/directdrop/src/App.jsx
@@ -6,13 +6,15 @@ function App() {
   
 
   return (
-    
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-blue-500">Hello, Tailwind CSS and Chakra UI!</h1>
-      <Button  size="sm" mt="4">
-        DirectDrop
-      </Button>
-    </div>  
+      <Router>
+        <Routes>
+         
+        </Routes>
+      </Router>
+    </div>
+    
+     
   )
 }
 
