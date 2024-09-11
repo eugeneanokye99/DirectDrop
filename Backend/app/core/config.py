2619d9e042f9d7bffcdaea59012e303d25e92ee6
Jmint <jeffreymintah737@gmail.com>
2024-09-11 14:10:26 +0000

    changes

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -5,12 +5,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # DB_USER: str
-    # DB_PASSWORD: str
-    # DB_HOST: str
-    # DB_PORT: str
-    # DB_NAME: str
-    
     SQLALCHEMY_DATABASE_URL: str
 
     class Config:
