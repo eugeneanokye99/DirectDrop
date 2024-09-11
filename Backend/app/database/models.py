5d5cb60ab15dfa659f23b3bf67eba81547744c3c
Jmint <jeffreymintah737@gmail.com>
2024-09-11 13:15:42 +0000

    changes

--- Backend/app/database/models.py
+++ Backend/app/database/models.py
@@ -1,4 +1,5 @@
 from app.database.db import Base
+from sqlalchemy.sql.expression
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from datetime import datetime
