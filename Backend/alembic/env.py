0d64a257b9aff4df9a07e45c65ebd2d1118bd0fd
Jmint <jeffreymintah737@gmail.com>
2024-09-11 14:01:24 +0000

    changes

--- Backend/alembic/env.py
+++ Backend/alembic/env.py
@@ -10,6 +10,9 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    'sqlalchemy.url' : {settings.SQLALCHEMY_DATABASE_URL}
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
