2e6956c06e27c11a99224fa17899d8381f1e989b
Jmint <jeffreymintah737@gmail.com>
2024-09-11 13:23:38 +0000

    changes

--- Backend/app/database/models.py
+++ Backend/app/database/models.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class User(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, pimary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     first_name = Column(String, unique=True, nullable=False)
     last_name = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
