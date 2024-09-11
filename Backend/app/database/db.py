ef59d711ae4a4ad69a3f62675d04bc35afe2244c
Jmint <jeffreymintah737@gmail.com>
2024-09-11 08:50:51 +0000

    changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -6,5 +6,6 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 import time
 from psycopg2.extras import RealDictCursor
+from app.core.config import settings
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/ecommerce"
