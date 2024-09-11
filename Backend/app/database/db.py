e41de57fd0aff6904b7adb845e57b15805995a91
Jmint <jeffreymintah737@gmail.com>
2024-09-11 08:48:58 +0000

    changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -1,2 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+import time
+from psycopg2.extras import RealDictCursor
