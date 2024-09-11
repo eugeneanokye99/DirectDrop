7e6505290fd56032b55d68233b9d8e38b248d052
Jmint <jeffreymintah737@gmail.com>
2024-09-11 13:04:47 +0000

    changes

--- Backend/app/database/models.py
+++ Backend/app/database/models.py
@@ -3,3 +3,7 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from datetime import datetime
 
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Clumnn(Integer, pimary_key=True, index=True)
