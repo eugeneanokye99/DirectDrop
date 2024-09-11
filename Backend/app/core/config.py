1da4cd446ae2c41752fe83cd62951b5191e5d347
Jmint <jeffreymintah737@gmail.com>
2024-09-11 08:52:42 +0000

    changes

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -1,4 +1,4 @@
-#
+
 # This file contains the configuration settings for the application.
 # The settings are loaded from the .env file using the pydantic_settings library. 
 # The settings are used to connect to the database and other configurations.
