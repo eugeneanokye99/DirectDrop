5a22fa1890bc57d9c3af01645b094fde075e8d05
Jmint <jeffreymintah737@gmail.com>
2024-09-11 09:17:27 +0000

    changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -4,9 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
-import time
-from psycopg2.extras import RealDictCursor
-from app.core.config import settings
+
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./DirectDrop.db" 
 
