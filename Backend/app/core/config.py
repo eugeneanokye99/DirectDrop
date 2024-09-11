f1f2e55be5c46122aaf1e354ee94474fa3a58a0d
Jmint <jeffreymintah737@gmail.com>
2024-09-11 14:14:19 +0000

    changes

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -2,12 +2,12 @@
 # The settings are loaded from the .env file using the pydantic_settings library. 
 # The settings are used to connect to the database and other configurations.
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, 
 
 class Settings(BaseSettings):
     SQLALCHEMY_DATABASE_URL: str
 
     class Config:
-        env_file = 'app/.env'
+        env_file = '.env'
         
 settings = Settings()
