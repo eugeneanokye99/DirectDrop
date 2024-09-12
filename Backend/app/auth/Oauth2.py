b5f8292244841e939c6ca7390d83a0f49cd84aa8
Jmint <jeffreymintah737@gmail.com>
2024-09-12 11:29:51 +0000

    changes

--- Backend/app/auth/Oauth2.py
+++ Backend/app/auth/Oauth2.py
@@ -24,7 +24,7 @@ def create_access_token(data:dict):
 def verify_access_token(token:str, credentials_exception):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: int = payload.get("user_id")
+        id: str = payload.get("user_id")
         if id is None:
             raise credentials_exception
         token_data = TokenData(id=id)
