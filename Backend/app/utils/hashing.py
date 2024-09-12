4de75db5bcbe911e2c96968e48383eb1ecb4e3c9
Jmint <jeffreymintah737@gmail.com>
2024-09-12 08:50:53 +0000

    change

--- Backend/app/utils/hashing.py
+++ Backend/app/utils/hashing.py
@@ -4,4 +4,5 @@ from passlib.context import CryptContext
 def hash(password:str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify_password(plain_password, ha)
