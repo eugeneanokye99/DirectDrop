482a2bfee2996206fe4f11affc356235bfeb44ef
Jmint <jeffreymintah737@gmail.com>
2024-09-12 08:50:57 +0000

    changes

--- Backend/app/utils/hashing.py
+++ Backend/app/utils/hashing.py
@@ -5,4 +5,5 @@ def hash(password:str):
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify_password(plain_password, ha)
+    return pwd_context.verify_password(plain_password, hashed_password)
+
