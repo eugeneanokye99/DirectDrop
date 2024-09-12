3ff58533b218fac156d03a45328b888b5bee77df
Jmint <jeffreymintah737@gmail.com>
2024-09-12 09:06:13 +0000

    changes

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+
+from app.auth import user_auth
 # from app.database.models import Base
 from app.database.db import engine
 
 app = FastAPI()
+app.include_router(user_auth.router)
 
 @app.get('/')
 def root():
