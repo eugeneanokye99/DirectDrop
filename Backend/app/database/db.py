df62fa7c9cf65a9a30d9380794c0271fdf180367
Jmint <jeffreymintah737@gmail.com>
2024-09-12 10:19:35 +0000

    Backend/app/database/db: changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+# from app.core.config import settings
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
@@ -20,12 +24,7 @@ def get_db():
     finally:
         db.close()
  
-try: 
-    with engine.connect() as connection:
-        print("Database connection successful")
-        
-except Exception as e:
-    print("Database connection failed", {e})
+
                 
 
     
