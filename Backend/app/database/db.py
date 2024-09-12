a3bbc345f358eaf0e3ddebddf8dd527d2d25feec
Jmint <jeffreymintah737@gmail.com>
2024-09-12 10:19:48 +0000

    changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'DirectDrop.db')}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
