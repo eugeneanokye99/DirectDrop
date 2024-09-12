13c89769e588555d5ea71bce153d4beec812ef69
Jmint <jeffreymintah737@gmail.com>
2024-09-12 10:55:05 +0000

    changes

--- Backend/alembic/env.py
+++ Backend/alembic/env.py
@@ -7,7 +7,13 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'DirectDrop.db')}"
 
 
 
