9a4baa42953116f79737fcce59ee6e688b6e2fe1
Jmint <jeffreymintah737@gmail.com>
2024-09-12 11:15:03 +0000

    change

--- Backend/app/auth/user_auth.py
+++ Backend/app/auth/user_auth.py
@@ -22,7 +22,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE  = 
+ACCESS_TOKEN_EXPIRE  = 60
 
 
 router = APIRouter(
