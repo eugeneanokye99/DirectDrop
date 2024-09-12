1ce7bd94bb96a24968004043b2772aa2d2645c67
Jmint <jeffreymintah737@gmail.com>
2024-09-12 08:28:35 +0000

    changes

--- Backend/app/auth/user_auth.py
+++ Backend/app/auth/user_auth.py
@@ -1,3 +1,4 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from app.database.models import User
-from app.database.
+from app.database.db import get_db
+from sqlalchemy.orm import Session
