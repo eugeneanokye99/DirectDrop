34ecc77c15d2ef199ebc0bf9a4f5914de6f137b3
Jmint <jeffreymintah737@gmail.com>
2024-09-12 11:35:05 +0000

    changes

--- Backend/app/auth/user_auth.py
+++ Backend/app/auth/user_auth.py
@@ -43,4 +43,7 @@ async def register_user(user:UserCreate, db: Session = Depends(get_db)):
     }
     
 @router.post("/login")
-async def login_user(user_credentialls: OAuth2PasswordRequestForm)
+async def login_user(
+    user_credentialls: OAuth2PasswordRequestForm = Depends(),
+    
+    )
