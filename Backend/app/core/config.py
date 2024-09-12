dacac37a0611b39f7638048be439360b99ae486b
Jmint <jeffreymintah737@gmail.com>
2024-09-12 11:13:23 +0000

    changes

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     
     SQLALCHEMY_DATABASE_URL: str
+    SECRET_KEY
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
