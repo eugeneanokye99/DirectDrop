98bd0e25133f7ff7350e856a9bc15db71bdc52d4
Jmint <jeffreymintah737@gmail.com>
2024-09-12 08:24:34 +0000

    changes

--- Backend/app/database/db.py
+++ Backend/app/database/db.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 
 
 
-SQLALCHEMY_DATABASE_URL = settings.
+SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
