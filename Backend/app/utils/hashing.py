ed01e7e9ba8581f7779d36a308e58200e8037863
Jmint <jeffreymintah737@gmail.com>
2024-09-12 12:05:07 +0000

    changes

--- Backend/app/utils/hashing.py
+++ Backend/app/utils/hashing.py
@@ -1,9 +1,15 @@
 from passlib.context import CryptContext
+import secrets
+
+# Generate a 32-byte secret key (for example, for JWT or encryption)
+secret_key = secrets.token_hex(32)
+print(secret_key)
+
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
-def hash(password:str):
+def hasher(password:str):
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
