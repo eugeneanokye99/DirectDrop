21d09b211f919696fbbb66eacf6041b83143cd24
Jmint <jeffreymintah737@gmail.com>
2024-09-13 08:43:48 +0000

    changes

--- Frontend/directdrop/src/pages/Register.jsx
+++ Frontend/directdrop/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import ( registerUser ) from "../services/Api.jsx";
+
+import {registerUser} from '../services/Api.jsx';
+
 import {
   Box,
   Button,
